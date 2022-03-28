158cb9d49f6d97d4bb5b96be5f3b719e64ebd76d
mirembe jemimah <jaymiecrystal117@gmail.com>
2022-03-28 17:38:55 +0300

    temperature gauge assignment

---
+++ assets/temp.js
@@ -0,0 +1,33 @@
+//temperature gauge
+const temp = 20;
+let patient = {
+    temperature: 67,
+};
+
+/* switch case
+switch (patient.temperature){
+    // conditions
+    case patient.temperature >= temp &&  patient.temperature <= 36:
+    console.log('Your temperature is normal')
+    break;
+    case patient.temperature >=37 && patient.temperature <= 40:
+        console.log('Your temperature is above normal')
+    break;
+    case patient.temperature >= 40: 
+        console.log('Your temperature is erratic')
+    break;
+    default:
+    console.log('Please see a doctor immediately!')
+*/ 
+
+if(patient.temperature >= temp && patient.temperature <= 36){
+    console.log('Your temperature is normal')
+} else if(patient.temperature >= 37 && patient.temperature <=40){
+    console.log('Your temperature is above normal')
+    
+} else if(patient.temperature >= 40){
+    console.log('Your temperature is erratic, Please see a doctor immediately!')
+}
+else {
+    console.log('The temperature is invalid')
+}
